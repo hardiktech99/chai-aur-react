9e0bd3251ad8a23015724795b670ce67eb1e432a
hardiktech99 <hardikpatel4314@gmail.com>
2025-02-18 15:17:06 +0530

    add new project

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,11 +2,11 @@ import Chai from "./chai"
 
 
 function App() {
-
+  const username = "chai aur hardik"
   return (
     <>
      <Chai/>
-      <h1>chai aur react</h1>
+      <h1>chai aur react + {username}</h1>
       <p>text para</p>
     </>
   )
---
+++ 02counter/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(0)
+
+  // let counter = 15
+
+  const addValue = () => {
+    setCounter((preValue) => preValue + 1)
+    
+  }
+
+  const removeValue = () => {
+    setCounter(counter-1)
+  }
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter Value : {counter}</h2>
+
+      <button onClick={addValue}>Add Value {counter}</button>
+      <button onClick={removeValue}>Remove Value {counter}</button>
+      <p>Footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
